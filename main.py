bd87d9a07eaccd95d46b8a74212937826ff47865
l1va <lev_matematik@tut.by>
2021-10-27 13:47:16 +0300

    add exc hook

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import sys
-
+import traceback
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication
 
 from src.settings import load_settings, sett, get_color
 from src.window import MainWindow
 from src.model import MainModel
 from src.controller import MainController
+
+
+def excepthook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    print("error catched!:")
+    print("error message:\n", tb)
+    with open("err.txt", "a+") as file1:
+        file1.write(tb + "\n")
+    QtWidgets.QApplication.quit()
+
 
 if __name__ == "__main__":
     load_settings()
@@ -14,12 +25,12 @@ if __name__ == "__main__":
     window = MainWindow()
     model = MainModel()
     cntrl = MainController(window, model)
-    #cntrl.load_stl("/home/l1va/Downloads/1_odn2.stl")  # TODO: removeme
+    # cntrl.load_stl("/home/l1va/Downloads/1_odn2.stl")  # TODO: removeme
     window.showMaximized()
     window.show()
-    try:
-        sys.exit(app.exec_())
-    except:
-        print("Error:", sys.exc_info())
-        with open("err.txt", "a+") as file1:
-            file1.write(str(sys.exc_info()))
+
+    # sys.exit(app.exec_())
+    sys.excepthook = excepthook
+    ret = app.exec_()
+    print("event loop exited")
+    sys.exit(ret)
