1e1b2ecbb747c70171f36e1c4208e750022c08d0
Lev Kozlov <42120385+lvjonok@users.noreply.github.com>
2022-09-19 08:40:57 +0300

    feat: update gcode parser (#33)
    * feat: update gcode parser
    
    * refactor: move to common function

--- src/gcode.py
+++ src/gcode.py
@@ -1,8 +1,25 @@
 import math
+import numpy as np
 from typing import List
 
+import src.settings
 from src import gui_utils
 import vtk
+
+def rotation_matrix(axis, theta):
+    """
+    Return the rotation matrix associated with counterclockwise rotation about
+    the given axis by theta radians.
+    """
+    axis = np.asarray(axis)
+    axis = axis / np.sqrt(np.dot(axis, axis))
+    a = np.cos(theta / 2.0)
+    b, c, d = -axis * np.sin(theta / 2.0)
+    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+    return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+                        [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+                        [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
 
 
 class GCode:
@@ -38,7 +55,7 @@ class Point:
         return res
 
 
-def parseArgs(args, x, y, z, a, b, absolute=True):
+def parseArgs(args, x, y, z, a, b, x_rot, absolute=True):
     xr, yr, zr, ar, br = 0, 0, 0, 0, 0
     if absolute:
         xr, yr, zr, ar, br = x, y, z, a, b
@@ -59,13 +76,18 @@ def parseArgs(args, x, y, z, a, b, absolute=True):
         elif arg[0] == ";":
             break
         elif arg[0] == "U":  # rotation around z of bed planer
+            import numpy as np
+            s = src.settings.sett()
+            rotationPoint = np.array(
+                [s.hardware.rotation_center_x, s.hardware.rotation_center_y, s.hardware.rotation_center_z])
+
             # convert from cylindrical coordinates to xyz
-            u = float(arg[1:])
-            r = xr
+            u = math.radians(float(arg[1:]))
+            r = yr
+            z = zr
 
-            xr = r * math.cos(u)
-            yr = r * math.sin(u)
-
+            cone_axis = rotation_matrix([1, 0, 0], np.radians(x_rot)).dot([0, 0, 1])
+            xr, yr, zr = rotation_matrix(cone_axis, -u).dot(np.array([0, r, z]) - rotationPoint) + rotationPoint
     if absolute:
         return xr, yr, zr, ar, br
     return xr + x, yr + y, zr + z, ar + a, br + b
@@ -146,10 +168,10 @@ def parseGCode(lines):
             elif args[0] == "G0":  # move to (or rotate)
                 if len(path) > 1:  # finish path and start new
                     layer.append(path)
-                x, y, z, a, b = parseArgs(args[1:], x, y, z, a, b, abs_pos)
+                x, y, z, a, b = parseArgs(args[1:], x, y, z, a, b, rotations[-1].x_rot, abs_pos)
                 path = [Point(x, y, z, a, b)]
             elif args[0] == "G1":  # draw to
-                x, y, z, a, b = parseArgs(args[1:], x, y, z, a, b, abs_pos)
+                x, y, z, a, b = parseArgs(args[1:], x, y, z, a, b, rotations[-1].x_rot, abs_pos)
                 path.append(Point(x, y, z, a, b))
             elif args[0] == "G90":  # absolute positioning
                 abs_pos = True
